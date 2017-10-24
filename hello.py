8424e99dd788bcc03bb701aa659d632eff87530b
蔡云泽 <294188197@qq.com>
2017-10-24 11:25:49 +0800

    提交代码

--- hello.py
+++ hello.py
@@ -1,15 +1,28 @@
-from flask.ext.moment import Moment
-from flask.ext.bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_bootstrap import Bootstrap
 from flask import render_template
 from flask import  Flask
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 
-@app.route('/')
-def index():
-    return render_template('base.html',name = 'caiyz'),201
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 if __name__=='__main__':
     app.run(debug=True)
